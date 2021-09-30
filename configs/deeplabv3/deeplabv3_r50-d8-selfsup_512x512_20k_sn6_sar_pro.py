2dc4c9212e733b072019973e1a767202ba7757c2
ChengShuaiLin <1106982578@qq.com>
2021-09-30 10:08:49 +0800

    n

--- configs/deeplabv3/deeplabv3_r50-d8-selfsup_512x512_20k_sn6_sar_pro.py
+++ configs/deeplabv3/deeplabv3_r50-d8-selfsup_512x512_20k_sn6_sar_pro.py
@@ -1,7 +1,7 @@
 '''
 Author: Shuailin Chen
 Created Date: 2021-09-14
-Last Modified: 2021-09-28
+Last Modified: 2021-09-29
 	content: 
 '''
 
@@ -11,6 +11,6 @@ _base_ = [
     '../_base_/schedules/schedule_20k.py'
 ]
 model = dict(
-    pretrained='/home/csl/code/PolSAR_SelfSup/work_dirs/pbyol_r50_sn6_sar_pro_ep200/20210927_094326/epoch_200_mmseg.pth',
+    pretrained='/home/csl/code/PolSAR_SelfSup/work_dirs/pbyol_r50_sn6_sar_pro_ul_ep200/20210929_102939/epoch_200.pth',
     
     decode_head=dict(num_classes=2), auxiliary_head=dict(num_classes=2))
