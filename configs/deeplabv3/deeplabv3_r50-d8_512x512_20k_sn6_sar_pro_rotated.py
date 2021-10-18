07a546cae9a60878b64168e18061c599dbb1174d
ChengShuaiLin <1106982578@qq.com>
2021-10-18 09:43:15 +0800

    add support of offline-rotated SAR-PRO

---
+++ configs/deeplabv3/deeplabv3_r50-d8_512x512_20k_sn6_sar_pro_rotated.py
@@ -0,0 +1,14 @@
+'''
+Author: Shuailin Chen
+Created Date: 2021-09-14
+Last Modified: 2021-10-16
+	content: 
+'''
+_base_ = [
+    '../_base_/models/deeplabv3_r50-d8.py',
+    '../_base_/datasets/sn6_sar_pro_rotated.py',
+    '../_base_/default_runtime.py',
+    '../_base_/schedules/schedule_20k.py'
+]
+model = dict(
+    decode_head=dict(num_classes=2), auxiliary_head=dict(num_classes=2))
