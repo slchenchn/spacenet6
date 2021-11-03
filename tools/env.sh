8e4abe1fe87fcd439ae785ed80f13ad53b4bf284
chenda <1106982578@qq.com>
2021-11-03 21:15:18 +0800

    n

---
+++ tools/env.sh
@@ -0,0 +1,12 @@
+# change conda environment and cuda version
+
+# cd ~/code/rsaicp_CD
+# source ~/.bashrc
+# source ~/shell_tools/switch_cuda.sh 11.0
+
+# export CUDA_HOME=:/usr/local/cuda-11.0
+# export LD_LIBRARY_PATH=:/usr/local/cuda-11.0/lib64
+# export PATH=:/usr/local/cuda-11.0/bin
+
+# conda init bash
+conda activate sn6
