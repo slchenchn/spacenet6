4909f964725b93de685b21c959d8b87c888879af
chenda <1106982578@qq.com>
2022-03-15 19:28:55 +0800

    add

--- configs/_base_/schedules/schedule_10k_le.py
+++ configs/_base_/schedules/schedule_10k_le.py
@@ -1,13 +1,13 @@
 '''
 Author: Shuailin Chen
 Created Date: 2021-09-14
-Last Modified: 2021-10-03
+Last Modified: 2022-03-15
 	content: only for linear evaluation
 '''
 
 # optimizer
-optimizer = dict(type='SGD', lr=0.01, momentum=0.9, weight_decay=0)
-# optimizer = dict(type='SGD', lr=0.001, momentum=0.9, weight_decay=0.0005)
+# optimizer = dict(type='SGD', lr=0.01, momentum=0.9, weight_decay=0)
+optimizer = dict(type='SGD', lr=0.001, momentum=0.9, weight_decay=0.0005)
 optimizer_config = dict()
 # learning policy
 lr_config = dict(policy='poly', power=0.9, min_lr=1e-4, by_epoch=False)
